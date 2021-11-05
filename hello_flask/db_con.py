9dd5367446f1e7ed69ecff9432e669f831d7389f
Jarrett Koran <jarrettkoran@gmail.com>
2021-11-04 17:01:35 -0700

    db update

--- hello_flask/db_con.py
+++ hello_flask/db_con.py
@@ -2,13 +2,13 @@ import psycopg2
 
 
 def get_db():
-    return psycopg2.connect(host="localhost", dbname="authme" , user="loki", password="4prez")
+    return psycopg2.connect(host="localhost", dbname="books" , user="jar", password="123")
 
-def get_db_instance():  
+def get_db_instance():
     db  = get_db()
     cur  = db.cursor( )
 
-    return db, cur 
+    return db, cur
 
 
 
@@ -21,7 +21,3 @@ if __name__ == "__main__":
 
     cur.execute("create table music ( song_name varchar(255), rating int);")
     db.commit()
-
-
-
-
