f32e4d73ce52b8bfd5a6ec8a5cbd4bf6d679c8fe
Jarrett Koran <jarrettkoran@gmail.com>
2021-12-14 21:51:46 -0800

    Update riotgames.py

--- hello_flask/riotgames.py
+++ hello_flask/riotgames.py
@@ -1,39 +1,16 @@
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
+import json
 
-api_key = 'RGAPI-166f7c8a-76f0-41d5-a269-c3fbddc4cc48'
-watcher = LolWatcher(api_key)
-my_region = 'na1'
 
-me = watcher.summoner.by_name(my_region, 'Doublelift')
-print(me)
+def summonerInfo(sumName):
+    api_key = 'RGAPI-166f7c8a-76f0-41d5-a269-c3fbddc4cc48'
+    watcher = LolWatcher(api_key)
+    my_region = 'na1'
 
-#def findSummoner(sumName):
+    me = watcher.summoner.by_name(my_region, sumName)
+    with open(me) as json_file:
+        data = json.load(json_file)
+    print(data)
 
-#    URL = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + sumName + "?api_key=RGAPI-166f7c8a-76f0-41d5-a269-c3fbddc4cc48"
-
-#    resp = request.get(URL)
-    #respJSON = resp.json()
-
-#    sumLevel = respJSON('summonerLevel')
-#
-#    ID = respJSON('id')
-#    ID = str(ID)
-
-#    URL2 = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + ID + "?api_key=RGAPI-5f73779d-d12c-45f1-ac61-788ba13a8409"
-
-#    resp2 = request.get(URL2)
-#    respJSON2 = resp2.json()
-
-#    highestChamp = respJSON2(0)("championId")
-#    highestChamp = str(highestChamp)
-
-    #champLevel = respJSON2(0)("championLevel")
-#    champLevel = str(champLevel)
-
-    #champMastery = respJSON2(0)("championPoints")
-#    champMastery = str(champMastery)
-
-#    sumInfo = [sumName, sumLevel,highestChamp,champLevel,champMastery]
-
-#    return sumInfo
+    #print(me)
