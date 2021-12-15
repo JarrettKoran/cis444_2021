a0af95e2a3df5c8d39a89239d61cd7339a63dc41
Jarrett Koran <jarrettkoran@gmail.com>
2021-12-14 22:41:06 -0800

    Update riotgames.py

--- hello_flask/riotgames.py
+++ hello_flask/riotgames.py
@@ -12,12 +12,8 @@ def summonerInfo(sumName):
     print(me)
 
 
-    print(me.get(0))
-    print(me.get(1))
-    print(me.get(2))
-    print(me.get(3))
-    print(me.get(4))
-    print(me.get(5))
-    print(me.get(6))
+    for i in me:
+        if i == 'summonerLevel':
+            print(me.get(i))
 
     return (me)
