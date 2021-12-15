f7d13469b16953d6d0924ddd5d0c683373f10a55
Jarrett Koran <jarrettkoran@gmail.com>
2021-12-14 22:53:26 -0800

    Update riotgames.py

--- hello_flask/riotgames.py
+++ hello_flask/riotgames.py
@@ -15,6 +15,6 @@ def summonerInfo(sumName):
     rankedStats = watcher.league.by_summoner(my_region,  summoner['id'])
     print(rankedStats)
 
-    allInfo = [summoner,rankedStats]
 
-    return (allInfo)
+
+    return ("working")
